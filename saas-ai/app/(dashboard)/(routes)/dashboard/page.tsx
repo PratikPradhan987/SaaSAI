210a762dbc0ec42784ec6323ce9672204898835c
Pratik Pradhan <71559227+PratikPradhan987@users.noreply.github.com>
2023-08-02 12:38:51 +0000

    adding clerk auth for user authentication

---
+++ saas-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const  DashboardPage = ( ) =>  {
+  return (
+    <>
+      DashBoard Page(Protected)
+    </>
+  )
+}
+export default DashboardPage
+
