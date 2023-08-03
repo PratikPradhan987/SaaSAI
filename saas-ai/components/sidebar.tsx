ebdb6199f3e4b1151f6ae83a6cbdaa6106f1ec76
PratikPradhan987 <pp3757634@gmail.com>
2023-08-03 19:25:43 +0530

    using pathname to highlight the sidebar elements

--- saas-ai/components/sidebar.tsx
+++ saas-ai/components/sidebar.tsx
@@ -2,6 +2,7 @@
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link"
+import {usePathname} from "next/navigation";
 import {
     LayoutDashboard,
     MessageSquare,
@@ -64,6 +65,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+    const pathname = usePathname();
+    // console.log(pathname)
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg -[#11827] text-white">
         <div className="px-3 py-2 flex-1">
@@ -82,7 +85,9 @@ const Sidebar = () => {
             <div className="space-y-1">
                 {routes.map((route) => (
                     <Link 
-                    className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+                    className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", 
+                    pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+                    )}
                     href={route.href}
                     key={route.href}
                     >
